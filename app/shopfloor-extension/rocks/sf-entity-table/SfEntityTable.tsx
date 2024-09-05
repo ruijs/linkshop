2670e91188e4b2e2bc8f22b7c43fb8213fc46716
Liu Peng <vslene@gmail.com>
2024-09-05 19:08:26 +0800

    fix entityStore select

--- app/shopfloor-extension/rocks/sf-entity-table/SfEntityTable.tsx
+++ app/shopfloor-extension/rocks/sf-entity-table/SfEntityTable.tsx
@@ -2,40 +2,55 @@ import { CommonProps, type Rock, type RockConfig } from "@ruiapp/move-style";
 import { renderRock } from "@ruiapp/react-renderer";
 import SfEntityTableMeta from "./SfEntityTableMeta";
 import type { SfEntityTableRockConfig, SfEntityTableState } from "./sf-entity-table-types";
-import { find, pick } from "lodash";
-import { generateRockConfigOfError, rapidAppDefinition, RapidEntity } from "@ruiapp/rapid-extension";
+import { pick } from "lodash";
+import { generateRockConfigOfError } from "@ruiapp/rapid-extension";
 
 export default {
+  onInit(context, props) {
+    const { pageSize = 20 } = props;
+    const entityConfig = props.entityConfig;
+    if (!entityConfig) {
+      return;
+    }
+
+    const dataSourceCode = entityConfig.name;
+    if (!context.scope.stores[dataSourceCode]) {
+      return;
+    }
+
+    // 初始化分页
+    const store = context.scope.stores[dataSourceCode] as any;
+    store.updateConfig({
+      pagination:
+        pageSize > 0
+          ? {
+            limit: pageSize,
+            offset: 0,
+          }
+          : undefined,
+    });
+  },
+
   Renderer(context, props, state) {
     const { pageSize = 20 } = props;
 
-    const entities = rapidAppDefinition.getEntities();
     const entityConfig = props.entityConfig;
-    let mainEntity: RapidEntity | undefined;
 
     const styleNames = [...CommonProps.PositionStylePropNames, ...CommonProps.SizeStylePropNames];
     const wrapStyle: React.CSSProperties = pick(props, styleNames) as any;
     wrapStyle.position = "absolute";
     wrapStyle.overflowY = "auto";
 
-    if (entityConfig?.entityCode) {
-      mainEntity = find(entities, (item) => item.code === entityConfig.entityCode);
-      if (!mainEntity) {
-        const errorRockConfig = generateRockConfigOfError(new Error(`Entitiy with code '${entityConfig.entityCode}' not found.`));
-        return renderRock({
-          context,
-          rockConfig: {
-            ...errorRockConfig,
-            style: wrapStyle,
-          },
-        });
-      }
+    if (!entityConfig) {
+      const errorRockConfig = generateRockConfigOfError(new Error(`Please select data source.`));
+      return renderRock({ context, rockConfig: { ...errorRockConfig, style: wrapStyle } });
     }
 
     const tableRockConfig: RockConfig = {
       $id: `${props.$id}-entity-list`,
       $type: "rapidEntityList",
-      dataSourceCode: entityConfig?.name,
+      dataSourceCode: entityConfig.name,
+      entityCode: entityConfig.entityCode,
       listActions: props.listActions,
       extraActions: props.extraActions,
       selectionMode: props.selectionMode,
